5d119158b2de6330c17ec18b014d75a277efc7c0
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2025-04-08 23:50:02 +0200

    Klokkeslett klasse

---
+++ client/src/objects/AktivitetKlokkeslett.ts
@@ -0,0 +1,63 @@
+class AktivitetKlokkeslett {
+    id: number;
+    title: string;
+    fra: string;
+    til: string;
+
+    private spaInteraction = (<any>window).spaInteraction; // Definert i main.ts
+
+    constructor(id : number, fra : string, til : string) {
+        this.id = id;
+        this.title = fra + " - " + til;
+        this.fra = fra;
+        this.til = til;
+    }
+
+    public async save() {
+        var data = {
+            action: 'UKMArrangementAdmin_ajax',
+            controller: 'aktivitet/klokkeslett/save',
+            id: this.id,
+            fra: this.fra,
+            til: this.til
+        };
+
+        var results = await this.spaInteraction.runAjaxCall('/', 'POST', data);
+
+        return results;
+    }
+
+    public async delete() {
+        var data = {
+            action: 'UKMArrangementAdmin_ajax',
+            controller: 'aktivitet/klokkeslett/delete',
+            id: this.id
+        };
+
+        var results = await this.spaInteraction.runAjaxCall('/', 'POST', data);
+
+        return results;
+    }
+
+    public async create() {
+        var data = {
+            action: 'UKMArrangementAdmin_ajax',
+            controller: 'aktivitet/klokkeslett/create',
+            id: this.id,
+            fra: this.fra,
+            til: this.til
+        };
+
+        var results = await this.spaInteraction.runAjaxCall('/', 'POST', data);
+
+        this.id = results.id;
+        this.fra = results.fra;
+        this.til = results.til;
+        this.title = results.fra + " - " + results.til;
+
+        return results;
+    }
+
+}
+
+export default AktivitetKlokkeslett;
