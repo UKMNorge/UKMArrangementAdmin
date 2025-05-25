d09e036f5cc793d84e8ebfead41e36cdb397f15d
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2025-05-26 00:23:18 +0200

    Henter alle gruppering med hendelser

---
+++ ajax/hendelser/getAlleHendelseGrupper.ajax.php
@@ -0,0 +1,41 @@
+<?php
+
+require_once('UKM/Autoloader.php');
+
+
+use UKMNorge\Arrangement\Program\HendelseGruppe;
+use UKMNorge\Arrangement\Arrangement;
+use UKMNorge\OAuth2\HandleAPICall;
+
+$handleCall = new HandleAPICall([],[], ['GET', 'POST'], false);
+
+$arrangement = null;
+
+try{
+    $arrangement = new Arrangement(get_option('pl_id'));
+
+} catch(Exception $e) {
+    if($e->getCode() == 401) {
+        $handleCall->sendErrorToClient($e->getMessage(), 401);
+    }
+    $handleCall->sendErrorToClient('Kunne ikke hente arrangementet', 401);
+}
+
+$hendelseGrupper = HendelseGruppe::getAlleByArrangement($arrangement);
+
+$retArr = [];
+
+foreach($hendelseGrupper as $gruppe) {
+    $gruppe->getHendelser();
+
+    $obj = [
+        'id' => $gruppe->getId(),
+        'navn' => $gruppe->getNavn(),
+        'beskrivelse' => $gruppe->getBeskrivelse(),
+        'hendelser' => $gruppe->getHendelser(),
+    ];
+
+    $retArr[] = $obj;
+}
+
+$handleCall->sendToClient($retArr);
